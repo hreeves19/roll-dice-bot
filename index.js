610473e2ca231f07de6ffa01162b304e54866963
Court Reeves <courtreeves6@gmail.com>
2021-07-26 17:25:04 -0400

    WIP .env error

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ const Bot = require('./utils/bot');
 const App = {
     client: new Discord.Client(),
     start: async () => {
-        await App.client.login(process.env.BOT_TOKEN);
+        try {
+            await App.client.login(process.env.BOT_TOKEN);
+            App.client.on("ready", () => {
+                console.log(`Logged in as ${App.client.user.tag}!`);
+            });
+    
+            App.client.on("message", msg => Bot.handleMessage(msg));
+        } catch (error) {
+            console.error(error);
+        }
         
-        App.client.on("ready", () => {
-            console.log(`Logged in as ${client.user.tag}!`);
-        });
-
-        App.client.on("message", msg => Bot.handleMessage(msg));
-        // App.client.on('error', (error) => Bot.handleError)
     }
 };
+
+App.start();
 
 module.exports = App;
